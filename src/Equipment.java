fddf04d1f6c84a25fcc0b8551f8c411ff5e339b6
Harry <harryhargreaves10@gmail.com>
2026-03-12 11:53:40 +0000

    Created initial Equipment class with test code.

---
+++ src/Equipment.java
@@ -0,0 +1,33 @@
+import java.util.Date;
+
+public class Equipment {
+    private String itemName;
+    private String manufacturer;
+    private String dateOfAcquisition;
+    private String type;
+    private boolean isAvailable;
+
+    public Equipment(String itemName, String manufacturer, String dateOfAcquisition, String type, boolean isAvailable) {
+        this.itemName = itemName;
+        this.manufacturer = manufacturer;
+        this.dateOfAcquisition = dateOfAcquisition;
+        this.type = type;
+        this.isAvailable = isAvailable;
+    }
+
+    public String getItemName(){
+        return this.itemName;
+    }
+    public String getManufacturer(){
+        return this.manufacturer;
+    }
+    public String getDateOfAcquisition(){
+        return this.dateOfAcquisition;
+    }
+    public String getType(){
+        return this.type;
+    }
+    public boolean getAvailability(){
+        return this.isAvailable;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
 void main(){
-    System.out.println("Hello world");
+    Equipment e = new Equipment("test", "test", "23/12/20", "test", false);
+//    System.out.println(e.getDateOfAcquisition());
+//    System.out.println(e.getAvailability());
+//    System.out.println(e.getItemName());
+//    System.out.println(e.getManufacturer());
+//    System.out.println(e.getType());
 }
